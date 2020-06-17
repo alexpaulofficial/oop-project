534d7c5869eaa3e821c207982a176247c0b11e14
Alessio <github@alexpaulofficial.com>
2020-06-18 01:25:26 +0200

    Sistemato "problema" filtro $bt e non solo
    Se l'utente inserisce i paramtri del filtro in senso decrescente, ciò non importa. In più è stata aggiunta la JavaDoc e sosno state sistemate altre piccole cose

--- src/main/java/it/univpm/GiAle/twitterProj/filters/Filter.java
+++ src/main/java/it/univpm/GiAle/twitterProj/filters/Filter.java
@@ -12,12 +12,14 @@ import com.google.gson.JsonElement;
 import it.univpm.GiAle.twitterProj.model.Tweet;
 
 public class Filter {
+	 * @return Lista filtrata
+	 */
 	public static ArrayList<Tweet> filterByLikes(ArrayList<Tweet> list, String filter, JsonElement param) {
 		// TODO Auto-generated method stub
-		ArrayList<Tweet> listaFiltrata = new ArrayList<Tweet>();
+		ArrayList<Tweet> filteredList = new ArrayList<Tweet>();
 		for (int i = 0; i < list.size(); i++) {
 			if (filter.equals("$gt") && list.get(i).getFavorite_count() > param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$gte") && list.get(i).getFavorite_count() >= param.getAsInt()) {
 				listaFiltrata.add(list.get(i));
@@ -26,91 +28,150 @@ public class Filter {
 				listaFiltrata.add(list.get(i));
 			}
 			if (filter.equals("$lte") && list.get(i).getFavorite_count() <= param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$bt")) {
 				if (!param.isJsonArray()) {
 					// eccezione
 					return null;
 				} else {
-					if (list.get(i).getFavorite_count() >= param.getAsJsonArray().get(0).getAsInt()
-							&& list.get(i).getFavorite_count() <= param.getAsJsonArray().get(1).getAsInt())
-						listaFiltrata.add(list.get(i));
+					/*
+					 * Se sono inseriti in ordine errato (prima un paramatro maggiore e poi un minore) viene
+					 * invertito il controllo
+					 */
+					if (param.getAsJsonArray().get(0).getAsInt() <= param.getAsJsonArray().get(1).getAsInt()) {
+						if (list.get(i).getFavorite_count() >= param.getAsJsonArray().get(0).getAsInt()
+								&& list.get(i).getFavorite_count() <= param.getAsJsonArray().get(1).getAsInt())
+							filteredList.add(list.get(i));
+					} else {
+						if (list.get(i).getFavorite_count() <= param.getAsJsonArray().get(0).getAsInt()
+								&& list.get(i).getFavorite_count() >= param.getAsJsonArray().get(1).getAsInt())
+							filteredList.add(list.get(i));
+					}
 				}
 
 			}
 		}
-		return listaFiltrata;
+
+		return filteredList;
 	}
 
+	/**
+	 * Filtra in base ai retweets (retweet_count)
+	 * 
+	 * @param list La lista da filtrare
+	 * @param filter Il JSON di richiesta dei filtri
+	 * @param param parametri dei filtri
+	 * @return Lista filtrata
+	 */
 	public static ArrayList<Tweet> filterByRetweet(ArrayList<Tweet> list, String filter, JsonElement param) {
 		// TODO Auto-generated method stub
-		ArrayList<Tweet> listaFiltrata = new ArrayList<Tweet>();
+		ArrayList<Tweet> filteredList = new ArrayList<Tweet>();
 		for (int i = 0; i < list.size(); i++) {
 			if (filter.equals("$gt") && list.get(i).getRetweet_count() > param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$gte") && list.get(i).getRetweet_count() >= param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$lt") && list.get(i).getRetweet_count() < param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$lte") && list.get(i).getRetweet_count() <= param.getAsInt()) {
-				listaFiltrata.add(list.get(i));
+				filteredList.add(list.get(i));
 			}
 			if (filter.equals("$bt")) {
 				if (!param.isJsonArray()) {
 					// eccezione
 					return null;
 				} else {
-					if (list.get(i).getFavorite_count() >= param.getAsJsonArray().get(0).getAsInt()
-							&& list.get(i).getFavorite_count() <= param.getAsJsonArray().get(1).getAsInt())
-						listaFiltrata.add(list.get(i));
+					if (param.getAsJsonArray().get(0).getAsInt() <= param.getAsJsonArray().get(1).getAsInt()) {
+						if (list.get(i).getRetweet_count() >= param.getAsJsonArray().get(0).getAsInt()
+								&& list.get(i).getRetweet_count() <= param.getAsJsonArray().get(1).getAsInt())
+							filteredList.add(list.get(i));
+					} else {
+						if (list.get(i).getRetweet_count() <= param.getAsJsonArray().get(0).getAsInt()
+								&& list.get(i).getRetweet_count() >= param.getAsJsonArray().get(1).getAsInt())
+							filteredList.add(list.get(i));
+					}
 				}
 
 			}
 		}
 
-		return listaFiltrata;
+		return filteredList;
 	}
 
+	/**
+	 * Filtra in base alla data di creazione del Tweet
+	 * 
+	 * Da notare che nel filtro gte si considera il giorno dato incluso Nel filtro
+	 * gt si considera il giorno dopo (lo stesso per lt e lte)
+	 *
+	 * @param list La lista da filtrare
+	 * @param filter Il JSON di richiesta dei filtri
+	 * @param param I parametri dei filtri
+	 * @return Lista filtrata
+	 * @throws ParseException nel caso di errore nel parsing della data
+	 */
 	public static ArrayList<Tweet> filterByTime(ArrayList<Tweet> list, String filter, JsonElement param)
 			throws ParseException {
 		// TODO Auto-generated method stub
-		ArrayList<Tweet> listaFiltrata = new ArrayList<Tweet>();
+		ArrayList<Tweet> filteredList = new ArrayList<Tweet>();
+		/*
+		 * E' stato scelto questo formato per il datepicker di Bootstrap per
+		 * l'interfaccia grafica
+		 */
 		SimpleDateFormat sf = new SimpleDateFormat("MMM dd, yyyy, HH:mm:ss", Locale.ENGLISH);
 		sf.setLenient(false);
 
 		if (filter.equals("$gte")) {
 			for (int i = 0; i < list.size(); i++) {
-				Date dataFiltro = sf.parse(param.getAsString());
-				if (list.get(i).parsedDate().compareTo(dataFiltro) > 0)
-					listaFiltrata.add(list.get(i));
+				Date dateFilter;
+
+				dateFilter = sf.parse(param.getAsString());
+				if (list.get(i).parsedDate().compareTo(dateFilter) > 0)
+					filteredList.add(list.get(i));
+
 			}
 		}
 		if (filter.equals("$gt")) {
 			for (int i = 0; i < list.size(); i++) {
-				Date dataFiltro = sf.parse(param.getAsString());
-				Calendar giorno_dopo = Calendar.getInstance();
-				giorno_dopo.setTime(dataFiltro);
-				giorno_dopo.add(Calendar.DATE, 1);
-				if (list.get(i).parsedDate().compareTo(giorno_dopo.getTime()) > 0)
-					listaFiltrata.add(list.get(i));
+				Date dateFilter;
+				dateFilter = sf.parse(param.getAsString());
+				/*
+				 * Per aumentare il giorno (e diminuire) si utilizza la classe calendar
+				 */
+
+				Calendar dateAfter = Calendar.getInstance();
+				dateAfter.setTime(dateFilter);
+				dateAfter.add(Calendar.DATE, 1);
+				if (list.get(i).parsedDate().compareTo(dateAfter.getTime()) > 0)
+					filteredList.add(list.get(i));
 			}
 		}
 		if (filter.equals("$lt")) {
 			for (int i = 0; i < list.size(); i++) {
-				Date dataFiltro = sf.parse(param.getAsString());
-				if (list.get(i).parsedDate().compareTo(dataFiltro) < 0)
-					listaFiltrata.add(list.get(i));
+				Date dateFilter;
+				dateFilter = sf.parse(param.getAsString());
+				
+				Calendar dateBefore = Calendar.getInstance();
+				dateBefore.setTime(dateFilter);
+				dateBefore.add(Calendar.DATE, -1);
+				
+				if (list.get(i).parsedDate().compareTo(dateFilter) < 0)
+					filteredList.add(list.get(i));
+
 			}
 		}
 		if (filter.equals("$lte")) {
 			for (int i = 0; i < list.size(); i++) {
-				Date dataFiltro = sf.parse(param.getAsString());
-				if (list.get(i).parsedDate().compareTo(dataFiltro) <= 0)
-					listaFiltrata.add(list.get(i));
+				Date dateFilter;
+				dateFilter = sf.parse(param.getAsString());
+				
+				if (list.get(i).parsedDate().compareTo(dateFilter) < 0)
+					filteredList.add(list.get(i));
+
 			}
 		}
 		if (filter.equals("$bt")) {
@@ -119,16 +180,32 @@ public class Filter {
 					// eccezione
 					return null;
 				} else {
-					Date dataFiltro1 = sf.parse(param.getAsJsonArray().get(0).getAsString());
-					Date dataFiltro2 = sf.parse(param.getAsJsonArray().get(1).getAsString());
+					Date dateFilter1;
+					try {
+						dateFilter1 = sf.parse(param.getAsJsonArray().get(0).getAsString());
+						Date dateFilter2 = sf.parse(param.getAsJsonArray().get(1).getAsString());
+						/**
+						 * Se le date da filtrare sono inserite in verso sbagliato si risolve
+						 * invertendole
+						 */
+						if (dateFilter1.compareTo(dateFilter2) > 0) {
+							Date tmp = dateFilter2;
+							dateFilter2 = dateFilter1;
+							dateFilter1 = tmp;
 
-					if (list.get(i).parsedDate().compareTo(dataFiltro1) > 0
-							&& list.get(i).parsedDate().compareTo(dataFiltro2) < 0)
-						listaFiltrata.add(list.get(i));
+						}
+						if (list.get(i).parsedDate().compareTo(dateFilter1) > 0
+								&& list.get(i).parsedDate().compareTo(dateFilter2) < 0)
+							filteredList.add(list.get(i));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
 				}
 			}
 		}
 
-		return listaFiltrata;
+		return filteredList;
 	}
 }
